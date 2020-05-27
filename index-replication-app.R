f75c9de1f0dec9ce66a98cb92f0eba4b0284194a
Jerzy Bednarski <jerzykb95@gmail.com>
2020-05-27 21:24:01 +0200

    Compare results of lasso and pca

--- index-replication-app.R
+++ index-replication-app.R
@@ -4,8 +4,9 @@ library(lubridate)
 library(stringr)
 
 source("helpers.R")
-source("pca_stock_selection.R")
 source("data_load.R")
+source("pca_stock_selection.R")
+source("lasso_stock_selection.R")
 
 ui <- navbarPage("Index replication",
                  tabPanel("About", 
@@ -16,10 +17,14 @@ ui <- navbarPage("Index replication",
                               selectInput("index", "Select index", choices = c("SPX", "DAX")),
                               dateRangeInput("dates", "Date range", start = today() - dyears(15)),
                               strong("PCA stock selection settings"),
-                              sliderInput("est_window", "Estimation window", min = 2*252, max= 5*252, value = 2*252),
-                              sliderInput("est_frequency", "Estimation frequency", min = 21, max= 12*21, value = 3*21),
+                              sliderInput("est_window_pca", "Estimation window", min = 2*252, max= 5*252, value = 2*252),
+                              sliderInput("est_frequency_pca", "Estimation frequency", min = 21, max= 12*21, value = 3*21),
                               sliderInput("stop_crit", "Stopping Criterion", min=0.5,max=1.5, value=0.7),
                               sliderInput("del_crit", "Deletion Criterion", min=0.7,max=1.2, value=1.),
+                              strong("Lasso stock selection settings"),
+                              sliderInput("est_window_lasso", "Estimation window", min = 2*252, max= 5*252, value = 2*252),
+                              sliderInput("est_frequency_lasso", "Estimation frequency", min = 21, max= 12*21, value = 3*21),
+                              sliderInput("lambda", "Lambda", min=0.001,max=0.01, value=0.005),
                               actionButton("go", "Go")),
                             mainPanel(plotOutput("simulation"))
                           )
@@ -35,13 +40,23 @@ server <- function(input, output, session){
     data
   })
   
-  getResults <- reactive({
-    results <- run_backtest(data = getData(), 
+  getPCAResults <- reactive({
+    run_backtest_pca(data = getData(), 
                             stopping_criterion = input$stop_crit, 
-                            deletion_criterion = input$del_crit, 
-                            est_window = input$est_window, 
-                            est_frequency = input$est_frequency)
+                             deletion_criterion = input$del_crit, 
+                             est_window = input$est_window_pca, 
+                             est_frequency = input$est_frequency_pca)
+    
   })
+  
+  getLassoResults <- reactive({
+    run_backtest_lasso(data = getData(), 
+                                        lambda = input$lambda,
+                                        est_window = input$est_window_lasso, 
+                                        est_frequency = input$est_frequency_lasso)
+  })
+  
+  getResults <- reactive({left_join(getPCAResults(), getLassoResults() %>% select(date, lasso), by="date")})
   
   buttonClick <- eventReactive(input$go, {
     getResults()
