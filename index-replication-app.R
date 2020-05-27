00c53fecee2c530bea7a62d3d3b234a9a856e7ea
Jerzy Bednarski <jerzykb95@gmail.com>
2020-05-27 20:38:22 +0200

    Allow different parameters for pca stock selection

--- index-replication-app.R
+++ index-replication-app.R
@@ -5,6 +5,7 @@ library(stringr)
 
 source("helpers.R")
 source("pca_stock_selection.R")
+source("data_load.R")
 
 ui <- navbarPage("Index replication",
                  tabPanel("About", 
@@ -12,19 +13,21 @@ ui <- navbarPage("Index replication",
                  tabPanel("Simulation",
                           sidebarLayout(
                             sidebarPanel(
-                              actionButton("go", "Go"),
-                              selectInput("index", h3("Select index"), choices = c("SPX", "DAX")),
-                              
-                              sliderInput("est_window", h3("Estimation window"), min = 2*252, max= 5*252, value = 2*252),
-                              sliderInput("est_frequency", h3("Estimation frequency"), min = 21, max= 12*21, value = 3*21),
-                              dateRangeInput("dates", h3("Date range"), start = today() - dyears(15))),
+                              selectInput("index", "Select index", choices = c("SPX", "DAX")),
+                              dateRangeInput("dates", "Date range", start = today() - dyears(15)),
+                              strong("PCA stock selection settings"),
+                              sliderInput("est_window", "Estimation window", min = 2*252, max= 5*252, value = 2*252),
+                              sliderInput("est_frequency", "Estimation frequency", min = 21, max= 12*21, value = 3*21),
+                              sliderInput("stop_crit", "Stopping Criterion", min=0.5,max=1.5, value=0.7),
+                              sliderInput("del_crit", "Deletion Criterion", min=0.7,max=1.2, value=1.),
+                              actionButton("go", "Go")),
                             mainPanel(plotOutput("simulation"))
                           )
-                          )
+                 )
 )
 
 
-server <- function(input, output){
+server <- function(input, output, session){
   
   getData <- reactive({
     data <- query_data(input$index, input$dates[1], input$dates[2])
@@ -33,8 +36,11 @@ server <- function(input, output){
   })
   
   getResults <- reactive({
-    results <- run_backtest(data = getData(), stopping_criterion = 0.7, deletion_criterion = 1., 
-                            est_window = input$est_window, est_frequency = input$est_frequency)
+    results <- run_backtest(data = getData(), 
+                            stopping_criterion = input$stop_crit, 
+                            deletion_criterion = input$del_crit, 
+                            est_window = input$est_window, 
+                            est_frequency = input$est_frequency)
   })
   
   buttonClick <- eventReactive(input$go, {
@@ -46,19 +52,7 @@ server <- function(input, output){
       gather("type", "return", -date) %>% 
       ggplot() +
       geom_line(aes(x=as.Date(date), y=return, color=type))
-    })
+  })
 }
-
-
-
-
-
-results <- run_backtest(data = data, stopping_criterion = 0.7, deletion_criterion = 1., est_window = 2* 252, est_frequency = 3*21)
-
-results %>% 
-  gather("type", "return", -date) %>% 
-  ggplot() +
-  geom_line(aes(x=as.Date(date), y=return, color=type))
-
 
 shinyApp(ui, server)
